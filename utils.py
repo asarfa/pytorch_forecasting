f235fafd73a4eb28a0107068e1f8c70ca9b484d5
Alban Sarfati <94382862+asarfa@users.noreply.github.com>
2024-05-29 12:42:47 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -246,7 +246,8 @@ def concat_sequences(
     if isinstance(sequences[0], rnn.PackedSequence):
         return rnn.pack_sequence(sequences, enforce_sorted=False)
     elif isinstance(sequences[0], torch.Tensor):
-        return torch.cat(sequences, dim=1)
+        sequences_ = [seq.view(len(seq)//14, 14) for seq in sequences]
+        return torch.cat(sequences_)
     elif isinstance(sequences[0], (tuple, list)):
         return tuple(
             concat_sequences([sequences[ii][i] for ii in range(len(sequences))]) for i in range(len(sequences[0]))
